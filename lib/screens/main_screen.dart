bdc6e20357cb17aa5b4e3dbb194874a65f43afd3
SoldierJP <soldierjp30@gmail.com>
2025-05-01 12:26:10 -0500

    FRONTEND FINAL FIX

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -6,17 +6,20 @@ import 'miseventos_screen.dart';
 import '../widgets/navbar.dart';
 
 class NavWrapper extends StatelessWidget {
-  final NavController navController = Get.put(NavController()); // Inject controller
+  final NavController navController = Get.put(
+    NavController(),
+  ); // Inject controller
 
-  final List<Widget> _screens = [
-    HomeScreen(),
-    MisEventosScreen(),
-  ];
+  final List<Widget> _screens = [HomeScreen(), MisEventosScreen()];
+
+  NavWrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Obx(() => _screens[navController.currentIndex.value]), // Reactive UI
+      body: Obx(
+        () => _screens[navController.currentIndex.value],
+      ), // Reactive UI
       bottomNavigationBar: Navbar(),
     );
   }
